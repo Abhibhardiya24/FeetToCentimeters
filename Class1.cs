5dcf460f976d8267b8d1cb2fa5845385b6ed574d
Abhibhardiya24 <abhishekbhardiya1995@gmail.com>
2023-08-31 18:26:18 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿namespace FeetToCentimeters
+{
+    public class Class1
+    {
+        class Height
+
+
+
+        {
+
+
+
+            static void main()
+
+
+
+            {
+
+
+
+                int feet = 5;
+
+
+
+                int inch = 7;
+
+
+
+                int total_Inch = (feet * 12) + inch;
+
+
+
+                double total_Centimeter = total_Inch * 2.54;
+
+
+
+                System.Console.WriteLine(total_Centimeter);
+
+
+
+                System.Console.ReadKey();
+
+
+
+            }
+
+
+
+        }
+    }
+}
